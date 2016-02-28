71770a72918f88a1ea3168fc1c2c23c583db8f04
Shawn Adderly <sadderly@gmail.com>
2016-02-27 21:34:34 -0500

    Updated Version

--- borrower/results/table.js
+++ borrower/results/table.js
@@ -1,3 +1,25 @@
+Table.ext.search.push(
+    function( settings, data, dataIndex ) {
+        var min = parseInt( $('#min').val(), 10 );
+        var max = parseInt( $('#max').val(), 10 );
+        var age = parseFloat( data[3] ) || 0; // use data for the age column
+ 
+        if ( ( isNaN( min ) && isNaN( max ) ) ||
+             ( isNaN( min ) && age <= max ) ||
+             ( min <= age   && isNaN( max ) ) ||
+             ( min <= age   && age <= max ) )
+        {
+            return true;
+        }
+        return false;
+    }
+);
+ 
 $(document).ready(function() {
-    $('#example').DataTable();
+    var table = $('#example').DataTable();
+     
+    // Event listener to the two range filtering inputs to redraw on input
+    $('#min, #max').keyup( function() {
+        table.draw();
+    } );
 } );
